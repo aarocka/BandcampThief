dc810d2b80ac8e4ca68827ea5500b0710e9af1cc
Aaron Kolker <akolker2@me.com>
2013-02-26 13:47:38 -0500

    Revert "Revert "Adding song downloading, still need to fix artist name fetching, added helper class""
    This reverts commit c18a4e48c6e2e1ad90f13179c65ff792d039ed6d.

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/query', search.query);
 app.get('/album', album.albumpage);
+app.get('/track', album.trackdownload);
 
 
 http.createServer(app).listen(app.get('port'), function(){
--- routes/albumpage.js
+++ routes/albumpage.js
@@ -1,20 +1,50 @@
-var request = require('request');
-var util = require('util');
+var util = require('util')
+  , request = require('request')
+  , helper = require('./helper');
 
 exports.albumpage = function(req, res) {
-	console.log('This would be the album page');
-	request('http://api.bandcamp.com/api/album/2/info?key=vatnajokull&album_id=' + req.query.album, function(error, response, body) {
-		var albumInfo = JSON.parse(body);
-
-		if (albumInfo.error) {
-			res.end('There was an error: ' +albumInfo.error_message);
+	helper.getAlbumInfo(req.query.album, function(albumInfo, error) {
+		if (error) {
+			res.end('There was an error: ' + error);
 			return;
 		}
+		console.log(albumInfo);
 
-		console.log(util.inspect(albumInfo));
 		res.render('album', {
 			title: 'album',
 			tracks: albumInfo.tracks
 		});
-	});
+	})
 };
+
+exports.trackdownload = function(req, res) {
+	helper.getSongInfo(req.query.track, function(trackInfo, error) {
+		if (error) {
+			res.end('There was an error: ' + trackInfo.error_message);
+		}
+
+		helper.getAlbumInfo(trackInfo.album_id, function(albumInfo, error) {
+			if (error) {
+				res.end('There was an error: ' + albumInfo.error_message);
+			}
+
+			for (var i = 0, l = albumInfo.tracks.length; i < l; i++) {
+				var temp = albumInfo.tracks[i];
+				if (temp.track_id == trackInfo.track_id) {
+					trackInfo = temp;
+					break;
+				}
+			}
+
+			res.header('Content-Type', 'application/octet-stream');
+			request.get(trackInfo.streaming_url).on('response', function(response) {
+				console.log(trackInfo);
+				res.header('Content-Length', response.headers['content-length']);
+				res.header('Last-Modified', response.headers['last-modified']);
+				res.header('Date', response.headers['date']);
+				res.header('Content-Disposition', 'attachment; filename=' + util.format("%s - %s - %s.mp3", trackInfo.artist, albumInfo.title, trackInfo.title));
+				response.pipe(res);
+			});
+		});
+	});
+}
