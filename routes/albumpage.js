5d0d7b00bc45e11c44758a50f0d823a2b9d8eb05
Aaron Kolker <akolker2@me.com>
2013-02-26 13:47:38 -0500

    Revert "Revert "Fixing album stuff""
    This reverts commit a59ab59d64dd1b9dc7af90f8b221e51bdda8bc97.

--- routes/albumpage.js
+++ routes/albumpage.js
@@ -1,3 +1,20 @@
-exports.albumpage = function(req, res){
+var request = require('request');
+var util = require('util');
+
+exports.albumpage = function(req, res) {
 	console.log('This would be the album page');
+	request('http://api.bandcamp.com/api/album/2/info?key=vatnajokull&album_id=' + req.query.album, function(error, response, body) {
+		var albumInfo = JSON.parse(body);
+
+		if (albumInfo.error) {
+			res.end('There was an error: ' +albumInfo.error_message);
+			return;
+		}
+
+		console.log(util.inspect(albumInfo));
+		res.render('album', {
+			title: 'album',
+			tracks: albumInfo.tracks
+		});
+	});
 };
