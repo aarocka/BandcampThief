15b807c28948998d4171ae6c6a3fda12dd25d047
Aaron Kolker <akolker2@me.com>
2013-02-26 12:26:27 -0500

    Revert "Successfully stealing tracks from album page"
    This reverts commit 577f88d4a212d3c5b5d19d566a7bf87aa97ab1b1.

--- routes/user.js
+++ routes/user.js
@@ -53,12 +53,11 @@ exports.query = function(req, res){
 			       	console.log('Album. show track list');
 			       	//res.redirect('/album/?album=' + urlInfoJSON.album_id);
 			       	request('http://api.bandcamp.com/api/album/2/info?key=vatnajokull&album_id=' + urlInfoJSON.album_id, function(error, response, body) {
-			       		var albumInfo = JSON.parse(body);
-			       		console.log(util.inspect(albumInfo));
+			       		albumInfo = JSON.parse(body);
 			       		
 			       		res.render('album', {
 			       			title: 'album',
-			       			tracks: albumInfo.tracks
+			       			tracks: albumInfo
 			       		});
 			       	});
 				}
