d1c9cf0be03f63ea56afbb6c053505531c955445
Aaron Kolker <akolker2@me.com>
2013-03-04 10:43:46 -0500

    Yea this works

---
+++ routes/albumpage.js
@@ -0,0 +1,55 @@
+var util = require('util')
+  , request = require('request')
+  , downloader = require('../lib/downloader')
+  , helper = require('../lib/helper');
+
+exports.albumpage = function(req, res) {
+	helper.getAlbumInfo(req.query.album, function(albumInfo, error) {
+		if (error) {
+			res.end('There was an error: ' + albumInfo.error_message);
+			return;
+		}
+		console.log(albumInfo);
+
+		res.render('album', {
+			title: 'album',
+			tracks: albumInfo.tracks
+		});
+		downloader.downloadAlbum(albumInfo.album_id);
+	});
+};
+
+exports.trackdownload = function(req, res) {
+	res.end('No Track Downloading');
+	return;
+	helper.getSongInfo(req.query.track, function(trackInfo, error) {
+		if (error) {
+			res.end('There was an error: ' + trackInfo.error_message);
+			return;
+		}
+
+		helper.getAlbumInfo(trackInfo.album_id, function(albumInfo, error) {
+			if (error) {
+				res.end('There was an error: ' + albumInfo.error_message);
+				return;
+			}
+
+			helper.getBandInfo(trackInfo.band_id, function(bandInfo, error) {
+				if (error) {
+					res.end('There was an error: ' + bandInfo.error_message);
+					return;
+				}
+
+				res.header('Content-Type', 'application/octet-stream');
+				request.get(trackInfo.streaming_url).on('response', function(response) {
+					console.log(trackInfo);
+					res.header('Content-Length', response.headers['content-length']);
+					res.header('Last-Modified', response.headers['last-modified']);
+					res.header('Date', response.headers['date']);
+					res.header('Content-Disposition', 'attachment; filename=' + util.format("%s - %s - %s.mp3", (trackInfo.artist ? trackInfo.artist : bandInfo.name), albumInfo.title, trackInfo.title));
+					response.pipe(res);
+				});
+			});
+		});
+	});
+}
