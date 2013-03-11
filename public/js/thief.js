64884a7e190f3cc8c989afcd397d24b40d22b476
Matt Bilker <me@mbilker.us>
2013-03-11 03:18:04 -0400

    Working on downloads system, upgraded bootstrap to dev release

--- public/js/thief.js
+++ public/js/thief.js
@@ -2,9 +2,16 @@ var socket;
 
 $(document).ready(function() {
   socket = io.connect();
+  done = "<i class='icon-ok'></i>  ";
 
   socket.on('progress', function(details) {
     //console.log(details);
-    $('#progressbar').css('width', details.progressSize + '%');
+    $('#progressbar').css('width', Math.floor((details.tracksCompleted / details.totalTracks) * 100) + '%');
+    //console.log('Finished: ' + details.tracksCompleted + ' / ' + details.totalTracks);
+  });
+  socket.on('finished', function(track) {
+    var li = $('li')[track.track.number - 1];
+    li.innerHTML = done + li.innerHTML;
+    //console.log(track.track.title);
   });
 });
