be5be0f6d4a2220f5e4732ead52752e5085f46d1
Matt Bilker <me@mbilker.us>
2013-03-13 18:24:00 -0400

    Fixed the progress bar system and made the downloader actually work instead of giving an undefined variable. Added experimental id3 tagger.

--- public/js/thief.js
+++ public/js/thief.js
@@ -4,19 +4,18 @@ $(document).ready(function() {
   socket = io.connect();
   done = "<i class='icon-ok'></i>  ";
 
-  socket.on(ID + ' catchup', function(details) {
+  socket.emit('catchup', { album_id: ID }, function(details) {
     for (var i = 0, l = details.finished.length; i < l; i++) {
       track = details.finished[i];
       var li = $('li')[track.track.number - 1];
       li.innerHTML = done + li.innerHTML;
     }
   });
-  socket.emit('catchup', { album_id: ID });
 
   socket.on(ID + ' progress', function(details) {
     //console.log(details);
-    $('#progressbar').css('width', Math.floor((details.tracksCompleted / details.totalTracks) * 100) + '%');
-    //console.log('Finished: ' + details.tracksCompleted + ' / ' + details.totalTracks);
+    //$('#progressbar').css('width', Math.floor((details.tracksCompleted / details.totalTracks) * 100) + '%');
+    $('#progressbar').css('width', details.progressSize + '%');
   });
   socket.on(ID + ' finished', function(track) {
     var li = $('li')[track.track.number - 1];
