a0549b316b0ab718dbb8f42f9e5433eeab28f0b0
Matt Bilker <me@mbilker.us>
2013-03-13 20:51:03 -0400

    Fixed the tagger and made the track tagging work by adding a patch

--- buildid3.sh
+++ buildid3.sh
@@ -1,7 +1,7 @@
+
 #!/bin/bash
 
 set -e
-set -x
 
 cd id3
 CWD=$(pwd)
@@ -21,16 +21,26 @@ else
 fi
 
 cd tmp
+echo "Untarring the archives"
 for i in $CWD/src/*.tar.gz; do tar zxf $i; done
 
 cd id3lib*
-for i in $CWD/patch/id3lib/*.patch; do patch -p1 < $i; done
-./configure --prefix=$CWD/bin
+echo "Patching id3lib"
+for i in $CWD/patch/id3lib/*.patch; do patch -p1 < $i >/dev/null 2>&1; done
+echo "Configuring id3lib"
+./configure --prefix=$CWD/bin >/dev/null 2>&1
+echo "Compiling id3lib"
 make
 make install
+echo "Finished Compiling"
 
 cd ..
 cd id3v2*
+echo "Patching id3v2"
+for i in $CWD/patch/id3v2/*.patch; do patch -p1 < $i; done
+echo "Compiling id3v2"
 make PREFIX=$CWD/bin
 mkdir -p $CWD/bin/man/man1
 make install PREFIX=$CWD/bin
+echo "Finished Compiling"
+echo "Finished"
