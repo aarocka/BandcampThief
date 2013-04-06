7b3080dbf8a658175e72d2de1d601008f1b34028
Matt Bilker <me@mbilker.us>
2013-04-06 16:32:55 -0400

    Using my git repository since it allows gyp to be run

--- libs/node-taglib/examples/simple.js
+++
@@ -1,8 +0,0 @@
-var Taglib = require(__dirname+"/../taglib")
-var util = require("util")
-
-for (var i = 2; i < process.argv.length; i++) {
-    Taglib.read(process.argv[i], function(err, tag, props) {
-        console.dir(err ? err : {'tag': tag, 'audioProperties': props});
-    });
-}
--- libs/node-taglib/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-node-gyp rebuild || exit 1
-npm link vows bindings
-./node_modules/vows/bin/vows --spec spec/taglibSpec.js spec/resolverSpec.js spec/buffersSpec.js
